899fb28569f88ee78e9fcce159c2b20cbafcf91b
Bhuwadol <9901139@ttbbank.com>
2025-06-28 15:44:48 +0700

    [Tiik][feat] add config

--- app/application.go
+++ app/application.go
@@ -1,17 +1,40 @@
 package app
 
-import "learning/app/controller"
+import (
+	"learning/app/controllers"
+	"learning/config"
+	"github.com/roonglit/credentials/pkg/credentials"
+	"github.com/gin-gonic/gin"
+)
 
 type Application struct {
-	Server *controller.Server
+	Server *controllers.Server
 }
 
 func New() *Application {
+	config := loadConfig()
+
+	server := controllers.New(
+		config,
+	)
+
 	return &Application{
-		Server: controller.New(),
+		Server: server,
 	}
 }
 
 func (app *Application) Run() {
 	app.Server.Run()
+}
+
+func loadConfig() *config.Config {
+	reader := credentials.NewConfigReader()
+
+	var config config.Config
+
+	if err := reader.Read(gin.Mode(), &config); err != nil {
+		panic("Failed to load configuration: " + err.Error())
+	}
+
+	return &config
 }
--- app/controllers/route.go
+++ app/controllers/route.go
@@ -1,4 +1,4 @@
-package controller
+package controllers
 
 func (s *Server) SetupRoutes() {
 	s.Router.GET("/", s.GetHealth)
